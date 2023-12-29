2513da454ed0b01ed2a70be50737e13f88ecb7fb
SamyCode <sambea2103@gmail.com>
2023-12-29 00:32:12 -0500

    Add followUser function to follow-service.ts

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,13 +1,28 @@
+import { Button } from '@/components/ui/button'
+import { isFollowingUser } from '@/lib/follow-service'
+import { getUserByUsername } from '@/lib/user-service'
+import { notFound } from 'next/navigation'
+
 interface UserPageProps {
   params: {
     username: string
   }
 }
 
-export default function UserPage ({ params: { username } }: UserPageProps) {
+export default async function UserPage ({ params: { username } }: UserPageProps) {
+  const user = await getUserByUsername(username)
+  if (!user) {
+    notFound()
+  }
+  const isFollowing = await isFollowingUser(user.id)
+  const label = isFollowing ? 'Unfollow' : 'Follow'
   return (
-    <section>
-      <h1 className="text-2xl font-bold">{username}</h1>
+    <section className='flex flex-col gap-y-4'>
+      <h1 className="text-2xl font-bold">{user?.username}</h1>
+      <p>id: {user.id}</p>
+      <article>
+        <Button>{label}</Button>
+      </article>
     </section>
   )
 }
--- lib/follow-service.ts
+++ lib/follow-service.ts
@@ -31,3 +31,45 @@ export const isFollowingUser = async (userId: string) => {
     return false
   }
 }
+
+export const followUser = async (userId: string) => {
+  const self = await getSelf()
+
+  const otherUser = await db.user.findUnique({
+    where: {
+      id: userId
+    }
+  })
+
+  if (!otherUser) {
+    throw new Error('User not found')
+  }
+
+  if (otherUser.id === self.id) {
+    throw new Error('You cannot follow yourself')
+  }
+
+  const existingFollow = await db.follow.findFirst({
+    where: {
+      followerId: self.id,
+      followingId: otherUser.id
+    }
+  })
+
+  if (existingFollow) {
+    throw new Error('You are already following this user')
+  }
+
+  const follow = await db.follow.create({
+    data: {
+      followerId: self.id,
+      followingId: otherUser.id
+    },
+    include: {
+      following: true,
+      follower: true
+    }
+  })
+
+  return follow
+}
