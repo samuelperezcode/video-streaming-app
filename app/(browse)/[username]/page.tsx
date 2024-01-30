72822940668bbf4e9e98be567d82371caaf44075
SamyCode <sambea2103@gmail.com>
2024-01-30 15:30:49 -0500

    fix: change recommended and following servies to exclude blocked users

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -2,6 +2,7 @@ import { isFollowingUser } from '@/lib/follow-service'
 import { getUserByUsername } from '@/lib/user-service'
 import { notFound } from 'next/navigation'
 import { Actions } from './_components/actions'
+import { isBlockedByUser } from '@/lib/block-service'
 
 interface UserPageProps {
   params: {
@@ -15,11 +16,13 @@ export default async function UserPage ({ params: { username } }: UserPageProps)
     notFound()
   }
   const isFollowing = await isFollowingUser(user.id)
+  const isBlocked = await isBlockedByUser({ id: user.id })
 
   return (
     <section className='flex flex-col gap-y-4'>
       <h1 className="text-2xl font-bold">{user?.username}</h1>
       <p>id: {user.id}</p>
+      <p>is blocked by this user: {`${isBlocked}`} </p>
       <article>
         <Actions
           isFollowing={isFollowing}
